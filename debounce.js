b37deefc132fbb36e85b4efcd36eac4f13e7b782
Gourab Konar <grbknr1996@gmail.com>
2022-07-03 10:16:24 +0530

    Update debounce.js

--- debounce.js
+++ debounce.js
@@ -14,6 +14,14 @@ function myDebounce(call, d) {
 }
 const BetterFunction = myDebounce(getData, 1000);
 
+
+/*
+Debouncing in JavaScript is a practice used to improve browser performance. 
+There might be some functionality in a web page which requires time-consuming computations. 
+If such a method is invoked frequently, it might greatly affect the performance of the browser,
+as JavaScript is a single threaded language. 
+*/
+
 const printStar = (num) => {
   let str = "";
   for (let index = 0; index < num; index++) {
